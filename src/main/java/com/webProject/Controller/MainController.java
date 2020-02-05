b5b0c270bf268bab7b7a96ccd9df235ca653a229
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-02-05 22:50:05 +0200

    add static resource(css file) in main page

--- src/main/java/com/webProject/Controller/MainController.java
+++ src/main/java/com/webProject/Controller/MainController.java
@@ -1,16 +1,21 @@
 package com.webProject.Controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.webProject.Daojpa.MessageDao;
 import com.webProject.Daojpa.UserDao;
 import com.webProject.entity.Message;
-import com.webProject.entity.User;
 
 @Controller
 
@@ -21,6 +26,9 @@ public class MainController {
 	
 	@Autowired
 	private UserDao userDao;
+	
+	@Value("${upload.path}")
+	private String uploadPath;
 	
 	
 	@GetMapping("/main")
@@ -41,10 +49,31 @@ public class MainController {
 	}
 	
 	@PostMapping("/main")
-	public String addNewMessage(@RequestParam String title, @RequestParam String tag,
-								 Model theModel) {
+	public String addNewMessage(@RequestParam String title,
+								@RequestParam String tag,
+								Model theModel,
+								@RequestParam("file") MultipartFile file) 
+	throws IllegalStateException, IOException {
 		
 		Message message = new Message(title, tag);
+		
+		
+		if(file != null && !file.getOriginalFilename().isEmpty()) {
+			File fileDir = new File(uploadPath);
+			
+			if(!fileDir.exists()) {
+				fileDir.mkdir();
+			}
+			
+			String uuidFile = UUID.randomUUID().toString();
+			
+			String resultFileName = uuidFile + "." + file.getOriginalFilename();
+			
+			file.transferTo(new File(uploadPath + "/" + resultFileName));
+			
+			message.setFilename(resultFileName);
+		}
+		
 		messageDao.save(message);
 		
 		Iterable<Message> messages = messageDao.findAll();
--- src/main/java/com/webProject/entity/Message.java
+++ src/main/java/com/webProject/entity/Message.java
@@ -18,6 +18,16 @@ public class Message {
 	
 	private String tag;
 	
+	private String filename;
+	
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
 	public Message() {}
 
 	public Message(String title, String tag) {
