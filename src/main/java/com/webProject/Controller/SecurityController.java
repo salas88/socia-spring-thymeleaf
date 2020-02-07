eb297942df5d7f97ad9aeb307683ac9ffb7e4cb0
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-02-07 22:06:09 +0200

    spring boot mail with gmail seccessfully

--- src/main/java/com/webProject/Controller/SecurityController.java
+++ src/main/java/com/webProject/Controller/SecurityController.java
@@ -1,22 +1,19 @@
-package com.webProject.Controller;
-
-import java.util.Collections;
+ package com.webProject.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.webProject.Daojpa.UserDao;
-import com.webProject.entity.Role;
 import com.webProject.entity.User;
+import com.webProject.service.UserService;
 
 @Controller
 public class SecurityController {
 	@Autowired
-	private UserDao userDao;
+	private UserService theUserService;
 	
 	@GetMapping("/registration")
 	public String getPageRegistation() {
@@ -25,18 +22,24 @@ public class SecurityController {
 	
 	@PostMapping("/registration")
 	public String registration(User user, Model theModel) {
-		
-		User userFromDb = userDao.findByUsername(user.getUsername());
-		
-		if(userFromDb != null) {
+	
+		if(!theUserService.addUser(user)) {
 			return "registration";
-		}
-		
-		user.setActive(true);
-		user.setRoles(Collections.singleton(Role.USER));
-		userDao.save(user);
-		
+		}	
 		return "redirect:/user";
 	}
 	
+	@GetMapping("/activate/{code}")
+	public String activatePage(Model theModel, @PathVariable String code) {
+		
+		boolean isActivate = theUserService.activateUsr(code);
+		
+		if(isActivate) 
+			theModel.addAttribute("message", "User successfully activated");
+		else
+			theModel.addAttribute("message", "Activation code is not found");
+			
+		
+		return "/login";
+	}
 }
--- src/main/java/com/webProject/service/UserService.java
+++ src/main/java/com/webProject/service/UserService.java
@@ -1,12 +1,17 @@
 package com.webProject.service;
 
+import java.util.Collections;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.webProject.Daojpa.UserDao;
+import com.webProject.entity.Role;
 import com.webProject.entity.User;
 
 @Service
@@ -14,11 +19,50 @@ public class UserService implements UserDetailsService{
 	
 	@Autowired
 	private UserDao userDao;
+	
+	@Autowired
+	private MailSender theMailSender;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userDao.findByUsername(username);
 		return user;
+	}
+	
+	public boolean addUser(User user) {
+		User userFromDb = userDao.findByUsername(user.getUsername());
+		
+		if(userFromDb != null) {
+			return false;}
+		
+		user.setActive(true);
+		user.setRoles(Collections.singleton(Role.USER));
+		user.setActivationCode(UUID.randomUUID().toString());
+		userDao.save(user); 
+		
+		if(!StringUtils.isEmpty(user.getEmail())) {
+			String message = String.format(
+					"Hello %s! \n"
+					+ "Welcome to App. Please visit next link: http://localhost:8080/activate/%s",
+							user.getUsername(),
+							user.getActivationCode());
+			
+			theMailSender.send(user.getEmail(), "Activation Code", message);
+		}
+		
+		return true;
+	}
+
+	public boolean activateUsr(String code) {
+		User user = userDao.findByActivationCode(code);
+		
+		if(user == null) {
+			return false;
+		}
+		
+		user.setActivationCode(null);
+		userDao.save(user);
+		return true;
 	}
 
 }
